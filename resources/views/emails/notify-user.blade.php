c40e8c0cc5d5ca5fbbb964b4a3a32da7f8122870
FIRST_NAME Bishank Badgami <bishank1993@gmail.com>
2022-08-08 01:02:03 +1000

    Fixing issues.

--- resources/views/emails/notify-user.blade.php
+++ resources/views/emails/notify-user.blade.php
@@ -9,15 +9,3 @@
 
 @endcomponent
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
-</head>
-<body>
-<a href="routines/create">Enter Daily Routine</a>
-</body>
-</html>
-
